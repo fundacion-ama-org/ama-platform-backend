ce792f7663c87f197a666fd0f3521c73511366ac
Wilmer Chavez <wechavez14@gmail.com>
2023-12-28 23:00:43 -0500

    feat: donors dto validations

--- PlatformAMA/Modules/Donors/DTOs/DonorUpdateDTO.cs
+++ PlatformAMA/Modules/Donors/DTOs/DonorUpdateDTO.cs
@@ -1,12 +1,24 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace PlatformAMA.Modules.Donors.DTOs
 {
   public class DonorUpdateDTO
   {
+    [Required]
+    [MaxLength(50)]
+    [MinLength(2)]
     public string FirstName { get; set; }
+    [Required]
+    [MaxLength(50)]
+    [MinLength(2)]
     public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    [Required]
+    [Phone]
     public string PhoneNumber { get; set; }
   }
 }
