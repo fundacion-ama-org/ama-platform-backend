4f4f5a4c0402e2fc11fbba066ff8be811588bae7
Wilmer Chavez <wechavez14@gmail.com>
2024-01-02 21:53:38 -0500

    feat: volunteers models implemented

---
+++ PlatformAMA/Modules/Volunteers/Models/Activity.cs
@@ -0,0 +1,14 @@
+
+
+namespace PlatformAMA.Modules.Volunteers.Models
+{
+  public class ActivityType
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedBy { get; set; }
+    public DateTime UpdatedBy { get; set; }
+  }
+}
